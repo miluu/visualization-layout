f252047281fec9e5bf846cc9f01400377daa40e1
huangyx <huangyx@gillion.com.cn>
2020-07-16 18:12:04 +0800

    更改 build 目录

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const paths = require('react-scripts-ts-antd/config/paths');
+
+paths.appBuild = path.join(__dirname, 'common');
+
 module.exports = function override(config, env) {
   return config;
 };
