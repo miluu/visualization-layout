986d43fe959085a107508e74071515d4cff0bc01
huangyx <huangyx@gillion.com.cn>
2021-02-01 16:12:14 +0800

    测试编译路径

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const paths = require('react-scripts-ts-antd/config/paths');
 
-paths.appBuild = path.join(__dirname, 'common');
+paths.appBuild = path.join(__dirname, 'common2');
 
 module.exports = function override(config, env) {
   return config;
